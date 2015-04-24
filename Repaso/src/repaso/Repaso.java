dd342a4d8d5ac851727ef21cec084c323f772f04
PC17 <PC17@MAESTRIA07-PC>
2015-04-24 11:45:21 -0500

    Nombre

--- Repaso/src/repaso/Repaso.java
+++ Repaso/src/repaso/Repaso.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package repaso;
 
-/**
- *
- * @author PC13
- */
+
 public class Repaso {
 
     /**
@@ -16,6 +9,9 @@ public class Repaso {
      */
     public static void main(String[] args) {
        System.out.println("erick");
+       for(int i=0;i<5;i++){
+            System.out.println("PAME ♥");
+        }
     }
     
 }
